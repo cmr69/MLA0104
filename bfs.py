33d32dbaacb6340778e0e10aaccb1ff7b7008afd
Chandra Mouleswara Rao <chanduchowdarythakkellapati@gmail.com>
2026-07-25 14:50:34 +0530

    Add files via upload

---
+++ bfs.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+graph = {
+    'A':['B','C'],
+    'B':['D','E'],
+    'C':['F'],
+    'D':[],
+    'E':[],
+    'F':[]
+}
+
+visited = set()
+queue = deque(['A'])
+
+while queue:
+    node = queue.popleft()
+
+    if node not in visited:
+        print(node)
+        visited.add(node)
+
+        for neighbour in graph[node]:
+            queue.append(neighbour)
